a0c182065d2e9d47bb7960c903e02030af38bc96
Carlos Prado <carlos.prado@portalsolar.com.br>
2020-05-09 15:20:41 -0300

    Add google map

--- javascript/index.js
+++ javascript/index.js
@@ -3,5 +3,5 @@ import StaysList from './stays-list.js';
 document.addEventListener('DOMContentLoaded', async function () {
   const staysList = new StaysList('São Paulo');
   const staysSection = document.querySelector('.stays');
-  staysSection.append(await staysList.element());
+  staysSection.prepend(await staysList.element());
 });
